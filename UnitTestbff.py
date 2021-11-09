b76ccb6eda8383b42154bf72cef2318b26eae8f0
Tina Chen <tvc5394@gmail.com>
2021-11-09 15:31:57 -0500

    Update UnitTestbff.py
    added two unit tests (for path_loop and get_all_paths_taken)

--- UnitTestbff.py
+++ UnitTestbff.py
@@ -35,6 +35,31 @@ class bffTest(unittest.TestCase):
                 ['B', 'B', 'Bo', 'o', 'ox', 'x', 'xo', 'o', 'oo', 'o', 'o']]
         self.Grid = ['o B x o o', 'o o o o o', 'o x o o o', 'o x o o x',
                      'o o x x o', 'B o x o o']
+        self.possible_grid =\
+            [['o', 'o', 'oB', 'B', 'Bx', 'x', 'xA', 'A', 'Ao', 'o', 'o'],
+                ['o', 'o', 'oB', 'B', 'Bx', 'x', 'xA', 'A', 'Ao', 'o', 'o'],
+                ['o', 'oo', 'oBoA', 'BA', 'BxAo', 'xo', 'xAoA', 'AA', 'AoAA',
+                    'oA', 'o'],
+                ['o', 'o', 'oA', 'A', 'Ao', 'o', 'oA', 'A', 'AA', 'A', 'A'],
+                ['o', 'oo', 'oAox', 'Ax', 'AoxA', 'oA', 'oAAo', 'Ao', 'AAoo',
+                    'Ao', 'A'],
+                ['o', 'o', 'ox', 'x', 'xA', 'A', 'Ao', 'o', 'oo', 'o', 'o'],
+                ['o', 'oA', 'oxAx', 'xx', 'xAxo', 'Ao', 'AooA', 'oA', 'ooAx',
+                    'ox', 'o'],
+                ['A', 'A', 'Ax', 'x', 'xo', 'o', 'oA', 'A', 'Ax', 'x', 'x'],
+                ['A', 'Ao', 'Axoo', 'xo', 'xoox', 'ox', 'oAxx', 'Ax', 'Axxo',
+                    'xo', 'x'],
+                ['o', 'o', 'oo', 'o', 'ox', 'x', 'xx', 'x', 'xo', 'o', 'o'],
+                ['o', 'oB', 'ooBo', 'oo', 'oxox', 'xx', 'xxxA', 'xA', 'xoAo',
+                    'oo', 'o'],
+                ['B', 'B', 'Bo', 'o', 'ox', 'x', 'xA', 'A', 'Ao', 'o', 'o'],
+                ['B', 'B', 'Bo', 'o', 'ox', 'x', 'xA', 'A', 'Ao', 'o', 'o']]
+        self.laser_pos =\
+            [(4, -1), (4, 1), (4, 3), (5, 0), (5, 2), (5, 4), (6, 1), (6, 3)]
+        self.refract_list = []
+        self.result_sort =\
+            [(4, 1), (4, 3), (5, 0), (5, 2), (5, 4), (6, 1), (6, 3)]
+
         self.Solution_Grid = ['o B x o o', 'o A o o o', 'A x o o A',
                               'o x A o x', 'A o x x A', 'B A x A o']
         self.P = [[6, 9], [9, 2]]
@@ -106,6 +131,20 @@ class bffTest(unittest.TestCase):
         self.assertTrue(grid_outcome(self.P, self.L,
                                      define_grid(self.sol_grid)),
                         'The incorrect grid outcome was outputted')
+
+    def test_get_all_paths_taken(self):
+        '''Checks to make sure that get_all_paths_taken returns all
+        of the correct paths taken by the laser'''
+        self.assertEqual(get_all_paths_taken(self.L, self.possible_grid),
+                         self.result_sort)
+
+    def test_path_loop(self):
+        '''Checks to make sure that path_loop returns the correct laser
+        positions and the correct unused refract list that arises from
+        hitting a C block'''
+        a, b = path_loop(self.L, self.possible_grid)
+        self.assertEqual(a, self.laser_pos)
+        self.assertEqual(b, self.refract_list)
 
 
 if __name__ == '__main__':
